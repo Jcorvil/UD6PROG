82b6e95d0e51295e17443d0b5d48016ce88f3e9f
Jcorvil <jcorvil600@g.educaand.es>
2023-03-20 13:01:43 +0100

    UD6

--- src/Ejemplos/EjemploDyV.java
+++ src/Ejemplos/EjemploDyV.java
@@ -5,21 +5,26 @@ import java.util.Arrays;
 public class EjemploDyV {
     public static void main(String[] args) {
         int[] a = {-2, -1, 3, 5, 8, 10};
-        int num = 3;
+        int num = 8;
         int posicion = dyv(a, num);
         System.out.println("El número " + num + " está en la posición: " + posicion);
     }
 
     private static int dyv(int[] a, int num) {
-        int medio = a.length / 2;
-        //Caso base
-        //Si el número que se busca está justo en el medio, ya se resuelve.
-        if (a[medio] == num){
-            return medio;
-        } else if (num < a[medio]) {
-            return dyv(Arrays.copyOfRange(a, 0, medio), num);
-        } else
-            return dyv(Arrays.copyOfRange(a, medio, a.length), num);
-
+        int pos = a.length / 2;
+        if (a.length == 0){
+            return -1;
+            //Caso base
+            //Si el numero dado está en el medio, se devuelve la posición directamente sin tener que realizar
+            //ninguna operación
+        } else if (num == a[pos]){
+            return pos;
+            //Si el numero dado se encuentra en la segunda mitad del array...
+        } else if (num > a[pos]){
+            //...
+            return pos + 1 + dyv(Arrays.copyOfRange(a, pos + 1, a.length), num);
+        } else {
+            return dyv(Arrays.copyOfRange(a, 0, pos), num);
+        }
     }
 }
